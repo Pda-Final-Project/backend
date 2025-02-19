229504123b2d7a1a7c199ca74dc9419c2ac182d7
greenlemonT <ms950219@naver.com>
2025-02-19 09:11:28 +0900

    [FEAT]: Eureka 서비스 등록

--- service-discovery/src/main/java/finpago/servicediscovery/ServiceDiscoveryApplication.java
+++ service-discovery/src/main/java/finpago/servicediscovery/ServiceDiscoveryApplication.java
@@ -2,8 +2,11 @@ package finpago.servicediscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableEurekaServer
 public class ServiceDiscoveryApplication {
 
     public static void main(String[] args) {

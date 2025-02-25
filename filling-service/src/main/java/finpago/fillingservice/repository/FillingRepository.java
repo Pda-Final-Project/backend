b8e37693e63655e60f283ad4949f5207fb510d8a
ham <tomy8964@naver.com>
2025-02-25 17:21:32 +0900

    [P3-56](#51) [CHORE]: 공시 리스트 조건 수정

--- filling-service/src/main/java/finpago/fillingservice/repository/FillingRepository.java
+++ filling-service/src/main/java/finpago/fillingservice/repository/FillingRepository.java
@@ -4,14 +4,25 @@ import finpago.fillingservice.entity.Filling;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 public interface FillingRepository extends JpaRepository<Filling, String> {
-    Page<Filling> findByFillingTickerOrderBySubmitTimestampDesc(String ticker, Pageable pageable);
-
     Optional<Filling> findByFillingId(String fillingId);
 
+    Page<Filling> findAllByOrderBySubmitTimestampDesc(Pageable pageable);
+
+    Page<Filling> findBySubmitTimestampBetweenOrderBySubmitTimestampDesc(String startDate, String endDate, Pageable pageable);
+
+    Page<Filling> findByFillingTypeOrderBySubmitTimestampDesc(String fillingType, Pageable pageable);
+
+    Page<Filling> findByFillingTickerOrderBySubmitTimestampDesc(String ticker, Pageable pageable);
+
+    Page<Filling> findByFillingTypeAndSubmitTimestampBetweenOrderBySubmitTimestampDesc(String fillingType, String startDate, String endDate, Pageable pageable);
+
+    Page<Filling> findByFillingTickerAndSubmitTimestampBetweenOrderBySubmitTimestampDesc(String ticker, String startDate, String endDate, Pageable pageable);
+
     Page<Filling> findByFillingTickerAndFillingTypeOrderBySubmitTimestampDesc(String ticker, String fillingType, Pageable pageable);
+
+    Page<Filling> findByFillingTickerAndFillingTypeAndSubmitTimestampBetweenOrderBySubmitTimestampDesc(String ticker, String fillingType, String startDate, String endDate, Pageable pageable);
 }

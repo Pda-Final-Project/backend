8796aa5a8fc424f34c11d4b0d73aca4b2e66b7db
ehdwoKIM <kinbell19@gmail.com>
2025-02-26 13:27:02 +0900

    [FIX] PinnedStock 추가 시 SecurityContextHolder 활용

---
+++ user-service/src/main/java/finpago/userservice/common/global/exception/error/DuplicatePinnedStockException.java
@@ -0,0 +1,7 @@
+package finpago.userservice.common.global.exception.error;
+
+public class DuplicatePinnedStockException extends RuntimeException {
+    public DuplicatePinnedStockException(String message) {
+        super(message);
+    }
+}
--- user-service/src/main/java/finpago/userservice/pinnedStock/repository/PinnedStockRepository.java
+++ user-service/src/main/java/finpago/userservice/pinnedStock/repository/PinnedStockRepository.java
@@ -1,10 +1,11 @@
 package finpago.userservice.pinnedStock.repository;
 
 import finpago.userservice.pinnedStock.entity.PinnedStock;
+import finpago.userservice.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface PinnedStockRepository extends JpaRepository<PinnedStock, String> {
-    Optional<PinnedStock> findByUserIdAndStockTicker(Long userId, String stockTicker);
+    Optional<PinnedStock> findByUserAndStockTicker(User user, String stockTicker);
 }

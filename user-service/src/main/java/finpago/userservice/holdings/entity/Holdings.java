22b88fa2864b5755c5d4ee80079028100ffb1e11
greenlemonT <ms950219@naver.com>
2025-02-26 16:28:53 +0900

    P3-62(#57) [FEAT]: 사용자 예수금과 보유주식 업데이트 배치처리

--- user-service/src/main/java/finpago/userservice/holdings/entity/Holdings.java
+++ user-service/src/main/java/finpago/userservice/holdings/entity/Holdings.java
@@ -1,4 +1,4 @@
-package finpago.userservice.holdings;
+package finpago.userservice.holdings.entity;
 
 import finpago.common.global.common.BaseEntity;
 import jakarta.persistence.*;
@@ -23,10 +23,11 @@ public class Holdings extends BaseEntity {
     @Column(name = "user_id", length = 64, nullable = false)
     private String userId;
 
-    //현재시세기준 보유종목 총금액
+    //보유 종목 수량
     @Column(name = "holding_quantity")
     private Long holdingQuantity;
 
+    //현재 시세 기준 보유종목 총 가격(이 종목의 현재시세 * 수량)
     @Column(name = "holding_total_price")
     private Long holdingTotalPrice;
 

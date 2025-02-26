2583c8389ed9f395457c394d7e1340448ea33267
ehdwoKIM <kinbell19@gmail.com>
2025-02-27 08:45:03 +0900

    [FIX] PinnedStockRepository Annotation 수정

--- user-service/src/main/java/finpago/userservice/pinnedStock/repository/PinnedStockRepository.java
+++ user-service/src/main/java/finpago/userservice/pinnedStock/repository/PinnedStockRepository.java
@@ -3,10 +3,12 @@ package finpago.userservice.pinnedStock.repository;
 import finpago.userservice.pinnedStock.entity.PinnedStock;
 import finpago.userservice.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface PinnedStockRepository extends JpaRepository<PinnedStock, String> {
 
     // 특정 관심 종목 조회

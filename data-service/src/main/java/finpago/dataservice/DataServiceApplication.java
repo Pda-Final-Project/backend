65b264d8411c20d5a77800204c142b3fd9e71cce
kwak <sal091625@gmail.com>
2025-02-26 14:56:30 +0900

    P3-60(#53) [FIX] ApiResponse 활용해서 api 명세서에 맞게 수정

--- data-service/src/main/java/finpago/dataservice/DataServiceApplication.java
+++ data-service/src/main/java/finpago/dataservice/DataServiceApplication.java
@@ -2,8 +2,10 @@ package finpago.dataservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DataServiceApplication {
 
     public static void main(String[] args) {

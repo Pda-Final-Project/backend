ddd30613a653226e3f4a025862e79942fc7296ff
ehdwoKIM <kinbell19@gmail.com>
2025-02-27 17:48:09 +0900

    [FIX] user-service 빌드시 Bean 미생성 오류 수정 완료

--- user-service/src/main/java/finpago/userservice/UserServiceApplication.java
+++ user-service/src/main/java/finpago/userservice/UserServiceApplication.java
@@ -2,18 +2,12 @@ package finpago.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableDiscoveryClient
+@EnableFeignClients
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "finpago.userservice.user.repository")
-@EntityScan(basePackages = "finpago.userservice.user.entity")
-@ComponentScan(basePackages = {"finpago.common", "finpago.userservice"})
 public class UserServiceApplication {
 
     public static void main(String[] args) {

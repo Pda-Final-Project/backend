f3c73d9e4a5ad9414a28d6d21f061ac8fe824d0b
kwak <sal091625@gmail.com>
2025-02-27 11:27:51 +0900

    P3-71(#68) [FEAT] ApiResponse 형식을 사용하도록 수정

--- data-service/src/main/java/finpago/dataservice/stock/controller/StockController.java
+++ data-service/src/main/java/finpago/dataservice/stock/controller/StockController.java
@@ -1,7 +1,10 @@
 package finpago.dataservice.stock.controller;
 
+import finpago.common.global.common.ApiResponse;
 import finpago.dataservice.stock.service.StockService;
 import finpago.dataservice.stock.service.StockSseService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,10 +25,12 @@ public class StockController {
     }
 
     @GetMapping
-    public List<Map<String, String>> getStocks(
+    public ResponseEntity<ApiResponse<List<Map<String, String>>>> getStocks(
         @RequestParam(required = false) String sortBy,
         @RequestParam(required = false) String searchParam) {
 
-        return stockService.getStocks(sortBy, searchParam);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ApiResponse.success(HttpStatus.OK, "종목 조회 성공", stockService.getStocks(sortBy, searchParam)));
     }
 }

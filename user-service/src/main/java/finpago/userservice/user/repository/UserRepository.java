d58110162ef15447d59d620939844620c83ac060
ehdwoKIM <kinbell19@gmail.com>
2025-02-27 13:49:12 +0900

    [FIX] user-service 빌드 오류 수정 중

--- user-service/src/main/java/finpago/userservice/user/repository/UserRepository.java
+++ user-service/src/main/java/finpago/userservice/user/repository/UserRepository.java
@@ -3,9 +3,11 @@ package finpago.userservice.user.repository;
 
 import finpago.userservice.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUserPhone(String userPhone);
 

2d8516892a7ad73220b51beba4e81c86457a1e36
ham <tomy8964@naver.com>
2025-03-06 20:13:26 +0900

    P3-101(#98) [ADD]: Discovery 어노테이션 prod 시 비활성화

--- user-service/src/main/java/finpago/userservice/UserServiceApplication.java
+++ user-service/src/main/java/finpago/userservice/UserServiceApplication.java
@@ -2,9 +2,11 @@ package finpago.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@ConditionalOnProperty(name = "spring.profiles.active", havingValue = "prod")
 @EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication(scanBasePackages = {"finpago.common.global", "finpago.userservice"})

bd6aca93604c8ee8001f83b76281e8a24e230db6
greenlemonT <ms950219@naver.com>
2025-03-06 21:15:08 +0900

    P3-109(#115) [FEAT]: 매도 체결 이벤트, 매수 체결 이벤트로 분리 및 실제 체결 데이터와의 비교를 통해 체결처리

---
+++ common/src/main/java/finpago/common/global/messaging/BuyTradeMatchEvent.java
@@ -0,0 +1,24 @@
+package finpago.common.global.messaging;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BuyTradeMatchEvent {
+    private UUID tradeId;       // 체결 고유 번호
+    private UUID buyOfferNumber;    // 매수 주문 ID
+    private Long buyerUserId;       // 매수자 ID
+    private String stockTicker; // 주식 티커
+    private Long tradeQuantity; // 체결 수량
+    private Long tradePrice;    // 체결 가격
+    private LocalDateTime tradeTimestamp; // 체결 시각
+    private Long buyerOrderQuantity; // 매수 주문 수량
+    private Float exchangeRate; // 환율
+    private Long buyerOfferPrice; // 매수 주문 가격
+}

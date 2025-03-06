303455c96cd25264d8477ea484d9839b2ea76e08
greenlemonT <ms950219@naver.com>
2025-03-06 09:54:54 +0900

    P3-102(#100) [ADD]: cors 비활성화

--- order-service/src/main/java/finpago/orderservice/order/config/auth/SecurityConfig.java
+++ order-service/src/main/java/finpago/orderservice/order/config/auth/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.disable())
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // ✅ 세션 사용 X
                 .authorizeHttpRequests(auth -> auth

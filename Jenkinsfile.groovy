be90a1476e95e3278a3a97da351f202bea0b9d3e
ham <tomy8964@naver.com>
2025-03-06 20:12:06 +0900

    P3-101(#98) [CHORE]: gardlew 빌드시 prod 프로필 적용

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -155,7 +155,7 @@ def buildAndPushDockerImage(serviceName) {
         sh 'echo "org.gradle.jvmargs=-Xms512m -Xmx2048m -Dfile.encoding=UTF-8 -XX:+HeapDumpOnOutOfMemoryError" > gradle.properties'
         sh 'echo "org.gradle.daemon.idleTimeout=60000" >> gradle.properties'
 
-        sh './gradlew bootJar --no-daemon --build-cache --parallel --continue'
+        sh './gradlew bootJar --no-daemon --build-cache -Pprod --parallel --continue -Dspring.profiles.active=prod'
         
         withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', usernameVariable: 'DOCKER_HUB_USER', passwordVariable: 'DOCKER_PASSWORD')]) {
             sh """

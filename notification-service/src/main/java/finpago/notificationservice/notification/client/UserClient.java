0e6e52999f3c768c515020679207ae0cc877c142
ehdwoKIM <kinbell19@gmail.com>
2025-03-07 10:53:47 +0900

    [FIX] 트레이딩 - 매칭 실제체결가 비교 로직 수정

--- notification-service/src/main/java/finpago/notificationservice/notification/client/UserClient.java
+++ notification-service/src/main/java/finpago/notificationservice/notification/client/UserClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 @FeignClient(name = "user-service", url = "http://user-service:19092")
 public interface UserClient {
 
-    @GetMapping("/v1/api/notification/status/{userId}")
-    Boolean getNotificationSetting(@PathVariable("userId") Long userId);
+    @GetMapping("/v1/api/notification/status")
+    Boolean getNotificationSetting();
 
 }

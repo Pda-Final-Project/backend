5cf823872803879cc202a30fa362e891c92bee12
ham <tomy8964@naver.com>
2025-03-07 13:37:40 +0900

    P3-101(#98) [CHORE]: Discovery

--- common/src/main/java/finpago/common/global/config/DiscoveryConfig.java
+++ common/src/main/java/finpago/common/global/config/DiscoveryConfig.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Profile("dev"
+@Profile("dev")
 @Configuration
 @ConditionalOnClass(EnableDiscoveryClient.class)
 @EnableDiscoveryClient

a548fda19acce7d9080516d5587292db63dd9a3a
ham <tomy8964@naver.com>
2025-03-07 13:47:25 +0900

    P3-101(#98) [CHORE]: common에서 관리

--- common/src/main/java/finpago/common/global/config/DiscoveryConfig.java
+++ common/src/main/java/finpago/common/global/config/DiscoveryConfig.java
@@ -1,6 +1,7 @@
 package finpago.common.global.config;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.Profile;
 @Profile("dev")
 @Configuration
 @ConditionalOnClass(EnableDiscoveryClient.class)
+@ConditionalOnProperty(name = "common.discovery.enabled", havingValue = "true", matchIfMissing = false)
 @EnableDiscoveryClient
 public class DiscoveryConfig {
 }

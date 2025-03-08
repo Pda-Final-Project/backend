bc88fcf7fa4f7a92212cbec421f8fd2d7cd72b5f
Geonwook Ham <tomy8964@naver.com>
2025-03-09 01:42:09 +0900

    P3-101(#98) [ADD]: Gateway Exception 추가

--- common/src/main/java/finpago/common/global/exception/handler/GlobalExceptionHandler.java
+++ common/src/main/java/finpago/common/global/exception/handler/GlobalExceptionHandler.java
@@ -10,9 +10,12 @@ import finpago.common.global.exception.dto.ErrorResponse;
 import finpago.common.global.exception.error.*;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
@@ -249,6 +252,13 @@ public class GlobalExceptionHandler {
 //
 //        return ResponseEntity.status(ErrorCode.INTERNAL_SERVER_ERROR.getStatus()).body(response);
 //    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleGeneralException(Exception ex, ServerWebExchange exchange) {
+        ServerHttpRequest request = exchange.getRequest();
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Error processing request: " + request.getURI() + ", Message: " + ex.getMessage());
+    }
 
     @ExceptionHandler(Exception.class)
     protected ResponseEntity<?> handleGeneralException(Exception exception, HttpServletRequest request) {

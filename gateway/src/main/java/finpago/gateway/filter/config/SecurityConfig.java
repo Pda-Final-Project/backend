47a972711478be0190c4e47736e3319e999aaaa8
ham <tomy8964@naver.com>
2025-03-10 14:14:57 +0900

    P3-101(#98) [ADD]: gateway cors 비활성화

--- gateway/src/main/java/finpago/gateway/filter/config/SecurityConfig.java
+++ gateway/src/main/java/finpago/gateway/filter/config/SecurityConfig.java
@@ -1,24 +1,23 @@
-
 package finpago.gateway.filter.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
-public class SecurityConfig{
+public class SecurityConfig {
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         http
-                .csrf(ServerHttpSecurity.CsrfSpec::disable); // CSRF 비활성화
+                .csrf(ServerHttpSecurity.CsrfSpec::disable) // CSRF 비활성화
+                .cors(ServerHttpSecurity.CorsSpec::disable) // CORS 비활성화
+                .authorizeExchange(exchanges -> exchanges
+                        .anyExchange().permitAll() // 모든 요청 허용
+                );
 
         return http.build();
     }

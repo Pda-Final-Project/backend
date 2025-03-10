2ce0a57a8d1c8e18f4ba9ab997722c12a17f59b2
greenlemonT <ms950219@naver.com>
2025-03-10 14:17:37 +0900

    P3-115(#126) [FIX]: 손익내역 계산 수정

--- execution-service/src/main/java/finpago/executionservice/ExecutionServiceApplication.java
+++ execution-service/src/main/java/finpago/executionservice/ExecutionServiceApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableScheduling
+@EnableJpaAuditing
 @ComponentScan(basePackages = {"finpago.common.global.exception", "finpago.executionservice"})
 public class ExecutionServiceApplication {
 

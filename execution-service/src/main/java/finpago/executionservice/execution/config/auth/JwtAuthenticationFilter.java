58179a4d1cd10f6df33c101b0b48ce654e5fb5e5
greenlemonT <ms950219@naver.com>
2025-03-11 17:17:29 +0900

    P3-120(#136) [FEAT]: SSE체결창 구현

--- execution-service/src/main/java/finpago/executionservice/execution/config/auth/JwtAuthenticationFilter.java
+++ execution-service/src/main/java/finpago/executionservice/execution/config/auth/JwtAuthenticationFilter.java
@@ -32,6 +32,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             filterChain.doFilter(request, response);
             return;
         }
+        if (request.getRequestURI().equals("/v1/api/trades/latest")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
 
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
--- execution-service/src/main/java/finpago/executionservice/execution/controller/TradeExecutionController.java
+++ execution-service/src/main/java/finpago/executionservice/execution/controller/TradeExecutionController.java
@@ -29,13 +29,6 @@ public class TradeExecutionController {
         return ResponseEntity.ok(latestTrades);
     }
 
-//    // SSE 연결 생성
-//    @GetMapping(value = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-//    public SseEmitter streamTradeUpdates() {
-//        System.out.println("============ 새로운 체결창 SSE 클라이언트 연결됨 =============");
-//        return tradeSseService.createEmitter();
-//    }
-
     /**
      * 특정 종목의 체결 내역 SSE 스트리밍
      */

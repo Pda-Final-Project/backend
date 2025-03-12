1bd68a51502ea3665a61552fd5c061110b069483
ham <tomy8964@naver.com>
2025-03-12 12:58:35 +0900

    P3-121(#142) [CHORE]: 도커 빌드 파일 수정

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -185,7 +185,7 @@ def buildAndPushPythonCrawler() {
         withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', usernameVariable: 'DOCKER_HUB_USER', passwordVariable: 'DOCKER_PASSWORD')]) {
             sh """
                 echo $DOCKER_PASSWORD | docker login -u $DOCKER_HUB_USER --password-stdin
-                docker build -t $DOCKER_HUB_USER/${PYTHON_CRAWLER_IMAGE}:${env.BUILD_NUMBER} .
+                docker build --no-cache -t $DOCKER_HUB_USER/${PYTHON_CRAWLER_IMAGE}:${env.BUILD_NUMBER} .
                 docker push $DOCKER_HUB_USER/${PYTHON_CRAWLER_IMAGE}:${env.BUILD_NUMBER}
             """
         }

c6cc0be80568abdf72131e1000e1e4da3875d588
greenlemonT <ms950219@naver.com>
2025-03-12 10:24:20 +0900

    P3-120(#136) [FIX]: 주문 테스트를 위한 수정

--- data-service/src/main/java/finpago/dataservice/exchangeRate/controller/ExchangeRateController.java
+++ data-service/src/main/java/finpago/dataservice/exchangeRate/controller/ExchangeRateController.java
@@ -12,7 +12,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import java.util.concurrent.TimeUnit;
 
 @RestController
-@RequestMapping("/api/exchange-rate")
+@RequestMapping("/v1/api/exchange-rate")
 @RequiredArgsConstructor
 public class ExchangeRateController {
 

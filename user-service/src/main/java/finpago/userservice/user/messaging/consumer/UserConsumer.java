87cfb875fa69b3a8b420a991d36f23c955382363
ehdwoKIM <kinbell19@gmail.com>
2025-03-13 16:22:31 +0900

    [FIX] userservice 모듈 UserConsumer null 데이터 방지 로직 추가

--- user-service/src/main/java/finpago/userservice/user/messaging/consumer/UserConsumer.java
+++ user-service/src/main/java/finpago/userservice/user/messaging/consumer/UserConsumer.java
@@ -30,8 +30,20 @@ public class UserConsumer {
     @KafkaListener(topics = "notice-topic", groupId = "notification-group")
     public void consumeNotification(ConsumerRecord<String, String> record) {
         try {
+            String message = record.value();
+
+            if (message == null || message.trim().isEmpty()) {
+                log.warn("수신된 Kafka 메시지가 비어 있음");
+                return;
+            }
+
             // JSON 문자열을 NoticeEvent 객체로 변환
             NoticeEvent noticeEvent = objectMapper.readValue(record.value(), NoticeEvent.class);
+
+            if (noticeEvent.getUserId() == null) {
+                log.warn("userId가 null입니다. 데이터 확인 필요: {}", message);
+                return;
+            }
 
             log.info("Kafka 알림 수신: {}", noticeEvent);
 

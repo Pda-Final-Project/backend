b4e633414ea0297485de097c8aca12c58b130bc9
ham <tomy8964@naver.com>
2025-03-14 23:59:22 +0900

    P3-101(#98) [CHORE]: 허용 도메인 수정

--- gateway/src/main/java/finpago/gateway/filter/config/SecurityConfig.java
+++ gateway/src/main/java/finpago/gateway/filter/config/SecurityConfig.java
@@ -37,9 +37,12 @@ public class SecurityConfig {
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true); // 인증 정보 포함 가능
+        config.setAllowCredentials(false); // 인증 정보 포함 가능
+//        config.setAllowedOrigins(List.of(
+//                "http://finpago-bucket.s3-website.ap-northeast-2.amazonaws.com" // S3 정적 호스팅 URL
+//        ));
         config.setAllowedOrigins(List.of(
-                "http://finpago-bucket.s3-website.ap-northeast-2.amazonaws.com" // S3 정적 호스팅 URL
+                "*"
         ));
         config.setAllowedHeaders(List.of("*")); // 모든 헤더 허용
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")); // 허용할 HTTP 메서드
